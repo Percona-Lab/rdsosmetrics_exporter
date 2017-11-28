4bbb59c34ad68cdc2077ce5997931e2a24de8791
Kamil Dziedzic <arvenil@klecza.pl>
2017-11-28 13:16:52 +0100

    PMM-1638: parallel everything * Scrape CloudWatch only when Prometheus scrapes exporter. * Scrape all instances independently, in parallel. * Scrape all instance metrics in parallel.

--- rdsosmetrics_exporter.go
+++ rdsosmetrics_exporter.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
 	"strconv"
 	"strings"
+	"sync"
 
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/common/log"
@@ -22,178 +24,314 @@ const (
 )
 
 type Exporter struct {
-	desc map[string]*prometheus.Desc
+	// For simplification assume this sync.Map always store values of type *prometheus.Desc.
+	// If for any reason this leads to problems feel free to add type checks to increase safety.
+	desc sync.Map
 }
 
-func NewExporter() (*Exporter, error) {
-	desc := make(map[string]*prometheus.Desc)
-
-	return &Exporter{
-		desc: desc,
-	}, nil
+func NewExporter() *Exporter {
+	return &Exporter{}
 }
 
 func (e *Exporter) Describe(ch chan<- *prometheus.Desc) {
-	for _, region := range endpoints.AwsPartition().Services()[endpoints.RdsServiceID].Regions() {
-		regionID := region.ID()
-		sess := session.Must(session.NewSession(&aws.Config{
-			Region: aws.String(regionID),
-		}))
-		svc := cloudwatchlogs.New(sess)
-		DescribeLogStreamsOutput, err := svc.DescribeLogStreams(&cloudwatchlogs.DescribeLogStreamsInput{
-			LogGroupName: aws.String(logGroupName),
-		})
-		if err != nil {
-			// region without RDS Enhanced Monitoring
-			continue
+	wg := sync.WaitGroup{}
+	defer wg.Wait()
+
+	regions := endpoints.AwsPartition().Services()[endpoints.RdsServiceID].Regions()
+	wg.Add(len(regions))
+	for _, region := range regions {
+		go func(regionID string) {
+			err := e.describeRegion(regionID)
+			if err != nil {
+				log.Error(err)
+			}
+			wg.Done()
+		}(region.ID())
+	}
+	wg.Wait()
+
+	e.desc.Range(
+		func(key, value interface{}) bool {
+			ch <- value.(*prometheus.Desc)
+			return true
+		},
+	)
+}
+
+func (e *Exporter) describeRegion(regionID string) error {
+	sess := session.Must(session.NewSession(&aws.Config{
+		Region: aws.String(regionID),
+	}))
+	svc := cloudwatchlogs.New(sess)
+	DescribeLogStreamsOutput, err := svc.DescribeLogStreams(&cloudwatchlogs.DescribeLogStreamsInput{
+		LogGroupName: aws.String(logGroupName),
+	})
+	if err != nil {
+		return fmt.Errorf("region '%s' is without RDS Enhanced Monitoring: %s", regionID, err)
+	}
+
+	wg := sync.WaitGroup{}
+	defer wg.Wait()
+	logStreams := DescribeLogStreamsOutput.LogStreams
+	wg.Add(len(logStreams))
+	for _, logStream := range logStreams {
+		go func(logStreamName *string) {
+			err := e.describeLogStream(logStreamName, svc)
+			if err != nil {
+				log.Error(err)
+			}
+			wg.Done()
+		}(logStream.LogStreamName)
+	}
+
+	return nil
+}
+
+func (e *Exporter) collectRegion(ch chan<- prometheus.Metric, regionID string) error {
+	sess := session.Must(session.NewSession(&aws.Config{
+		Region: aws.String(regionID),
+	}))
+	svc := cloudwatchlogs.New(sess)
+	DescribeLogStreamsOutput, err := svc.DescribeLogStreams(&cloudwatchlogs.DescribeLogStreamsInput{
+		LogGroupName: aws.String(logGroupName),
+	})
+	if err != nil {
+		// region without RDS Enhanced Monitoring
+		return fmt.Errorf("region '%s' is without RDS Enhanced Monitoring: %s", regionID, err)
+	}
+
+	wg := sync.WaitGroup{}
+	defer wg.Wait()
+	logStreams := DescribeLogStreamsOutput.LogStreams
+	wg.Add(len(logStreams))
+	for _, logStream := range logStreams {
+		go func(logStreamName *string) {
+			err := e.collectLogStream(ch, regionID, logStreamName, svc)
+			if err != nil {
+				log.Error(err)
+			}
+			wg.Done()
+		}(logStream.LogStreamName)
+	}
+
+	return nil
+}
+
+func (e *Exporter) describeLogStream(logStreamName *string, svc *cloudwatchlogs.CloudWatchLogs) error {
+	GetLogEventsOutput, err := svc.GetLogEvents(&cloudwatchlogs.GetLogEventsInput{
+		Limit:         aws.Int64(1),
+		LogGroupName:  aws.String(logGroupName),
+		LogStreamName: logStreamName,
+	})
+	if err != nil {
+		return err
+	}
+
+	var message interface{}
+	err = json.Unmarshal([]byte(*GetLogEventsOutput.Events[0].Message), &message)
+	if err != nil {
+		return err
+	}
+
+	wg := sync.WaitGroup{}
+	defer wg.Wait()
+
+	messages, ok := message.(map[string]interface{})
+	if !ok {
+		return fmt.Errorf("unsupported message type: %T", message)
+	}
+	wg.Add(len(messages))
+	for key, value := range messages {
+		go func(key string, value interface{}) {
+			err := e.describeValue(key, value)
+			if err != nil {
+				log.Error(err)
+			}
+			wg.Done()
+		}(key, value)
+	}
+
+	return nil
+}
+
+func (e *Exporter) describeValue(key string, value interface{}) error {
+	switch v := value.(type) {
+	case float64:
+		e.addDesc("", key)
+	case map[string]interface{}:
+		for kkey, vvvalue := range v {
+			switch vvvalue.(type) {
+			case float64:
+				e.addDesc(key, kkey)
+			}
 		}
-
-		for _, LogStream := range DescribeLogStreamsOutput.LogStreams {
-			GetLogEventsOutput, err := svc.GetLogEvents(&cloudwatchlogs.GetLogEventsInput{
-				Limit:         aws.Int64(1),
-				LogGroupName:  aws.String(logGroupName),
-				LogStreamName: LogStream.LogStreamName,
-			})
-			if err != nil {
-				log.Fatalln(err)
-			}
-
-			var message interface{}
-			err = json.Unmarshal([]byte(*GetLogEventsOutput.Events[0].Message), &message)
-			if err != nil {
-				// not JSON? skipping
-				continue
-			}
-
-			for key, value := range message.(map[string]interface{}) {
-				switch vvalue := value.(type) {
-				case float64:
-					e.addDesc("", key)
-				case map[string]interface{}:
-					for kkey, vvvalue := range vvalue {
-						switch vvvalue.(type) {
-						case float64:
-							e.addDesc(key, kkey)
-						}
-					}
-				case []interface{}:
-					for i, u := range vvalue {
-						switch vvvalue := u.(type) {
-						case map[string]interface{}:
-							for kkey, vvvvalue := range vvvalue {
-								switch vvvvalue.(type) {
-								case float64:
-									e.addDesc(key, strconv.Itoa(i)+"_"+kkey)
-								}
-							}
-						}
+	case []interface{}:
+		for i, u := range v {
+			switch vvvalue := u.(type) {
+			case map[string]interface{}:
+				for kkey, vvvvalue := range vvvalue {
+					switch vvvvalue.(type) {
+					case float64:
+						e.addDesc(key, strconv.Itoa(i)+"_"+kkey)
 					}
 				}
 			}
 		}
+	case string:
+		switch key {
+		case
+			"engine",
+			"instanceID",
+			"instanceResourceID",
+			"timestamp",
+			"uptime":
+			// skipping those values
+		default:
+			return fmt.Errorf("unsupported key '%s' when describing value", key)
+		}
+	default:
+		return fmt.Errorf("unsupported value type '%T' for key '%s' when describing value", value, key)
 	}
 
-	for _, v := range e.desc {
-		ch <- v
-	}
+	return nil
 }
 
 func (e *Exporter) addDesc(subsystem string, name string) {
 	FQName := prometheus.BuildFQName(namespace, subsystem, name)
-	if _, ok := e.desc[FQName]; !ok {
-		e.desc[FQName] = prometheus.NewDesc(
+
+	if _, ok := e.desc.Load(FQName); ok {
+		return
+	}
+
+	e.desc.Store(
+		FQName,
+		prometheus.NewDesc(
 			strings.ToLower(FQName),
-			"Authomatially parsed metric from "+logGroupName+" Log Group",
+			"Automatically parsed metric from "+logGroupName+" Log Group",
 			[]string{
 				"region",
 				"instanceID",
 			},
 			nil,
-		)
-	}
+		),
+	)
 }
 
 func (e *Exporter) updateMetric(ch chan<- prometheus.Metric, region string, instanceID string, subsystem string, name string, value float64) {
 	FQName := prometheus.BuildFQName(namespace, subsystem, name)
-	ch <- prometheus.MustNewConstMetric(
-		e.desc[FQName],
-		prometheus.UntypedValue,
-		value,
-		region,
-		instanceID,
-	)
+	if v, ok := e.desc.Load(FQName); ok {
+		ch <- prometheus.MustNewConstMetric(
+			v.(*prometheus.Desc),
+			prometheus.UntypedValue,
+			value,
+			region,
+			instanceID,
+		)
+	}
 }
 
 func (e *Exporter) Collect(ch chan<- prometheus.Metric) {
-	for _, region := range endpoints.AwsPartition().Services()[endpoints.RdsServiceID].Regions() {
-		regionID := region.ID()
-		sess := session.Must(session.NewSession(&aws.Config{
-			Region: aws.String(regionID),
-		}))
-		svc := cloudwatchlogs.New(sess)
-		DescribeLogStreamsOutput, err := svc.DescribeLogStreams(&cloudwatchlogs.DescribeLogStreamsInput{
-			LogGroupName: aws.String(logGroupName),
-		})
-		if err != nil {
-			// region without RDS Enhanced Monitoring
-			continue
+	wg := sync.WaitGroup{}
+	defer wg.Wait()
+
+	regions := endpoints.AwsPartition().Services()[endpoints.RdsServiceID].Regions()
+	wg.Add(len(regions))
+	for _, region := range regions {
+		go func(regionID string) {
+			err := e.collectRegion(ch, regionID)
+			if err != nil {
+				log.Error(err)
+			}
+			wg.Done()
+		}(region.ID())
+	}
+}
+
+func (e *Exporter) collectLogStream(ch chan<- prometheus.Metric, regionID string, logStreamName *string, svc *cloudwatchlogs.CloudWatchLogs) error {
+	GetLogEventsOutput, err := svc.GetLogEvents(&cloudwatchlogs.GetLogEventsInput{
+		Limit:         aws.Int64(1),
+		LogGroupName:  aws.String(logGroupName),
+		LogStreamName: logStreamName,
+	})
+	if err != nil {
+		return err
+	}
+
+	var message interface{}
+	err = json.Unmarshal([]byte(*GetLogEventsOutput.Events[0].Message), &message)
+	if err != nil {
+		return err
+	}
+
+	wg := sync.WaitGroup{}
+	defer wg.Wait()
+
+	messages, ok := message.(map[string]interface{})
+	if !ok {
+		return fmt.Errorf("unsupported message type: %T", message)
+	}
+	wg.Add(len(messages))
+	instanceID := messages["instanceID"].(string)
+	for key, value := range messages {
+		go func(key string, value interface{}) {
+			err := e.collectValue(ch, regionID, instanceID, key, value)
+			if err != nil {
+				log.Error(err)
+			}
+			wg.Done()
+		}(key, value)
+	}
+
+	return nil
+}
+
+func (e *Exporter) collectValue(ch chan<- prometheus.Metric, regionID string, instanceID string, key string, value interface{}) error {
+	switch v := value.(type) {
+	case float64:
+		e.updateMetric(ch, regionID, instanceID, "", key, v)
+	case map[string]interface{}:
+		for kkey, vvvalue := range v {
+			switch vvvvalue := vvvalue.(type) {
+			case float64:
+				e.updateMetric(ch, regionID, instanceID, key, kkey, vvvvalue)
+			}
 		}
-
-		for _, LogStream := range DescribeLogStreamsOutput.LogStreams {
-			GetLogEventsOutput, err := svc.GetLogEvents(&cloudwatchlogs.GetLogEventsInput{
-				Limit:         aws.Int64(1),
-				LogGroupName:  aws.String(logGroupName),
-				LogStreamName: LogStream.LogStreamName,
-			})
-			if err != nil {
-				log.Fatalln(err)
-			}
-
-			var message interface{}
-			err = json.Unmarshal([]byte(*GetLogEventsOutput.Events[0].Message), &message)
-			if err != nil {
-				// not JSON? skipping
-				continue
-			}
-
-			instanceID := message.(map[string]interface{})["instanceID"].(string)
-			for key, value := range message.(map[string]interface{}) {
-				switch vvalue := value.(type) {
-				case float64:
-					e.updateMetric(ch, regionID, instanceID, "", key, vvalue)
-				case map[string]interface{}:
-					for kkey, vvvalue := range vvalue {
-						switch vvvvalue := vvvalue.(type) {
-						case float64:
-							e.updateMetric(ch, regionID, instanceID, key, kkey, vvvvalue)
-						}
-					}
-				case []interface{}:
-					for i, u := range vvalue {
-						switch vvvalue := u.(type) {
-						case map[string]interface{}:
-							for kkey, vvvvalue := range vvvalue {
-								switch vvvvvalue := vvvvalue.(type) {
-								case float64:
-									e.updateMetric(ch, regionID, instanceID, key, strconv.Itoa(i)+"_"+kkey, vvvvvalue)
-								}
-							}
-						}
+	case []interface{}:
+		for i, u := range v {
+			switch vvvalue := u.(type) {
+			case map[string]interface{}:
+				for kkey, vvvvalue := range vvvalue {
+					switch vvvvvalue := vvvvalue.(type) {
+					case float64:
+						e.updateMetric(ch, regionID, instanceID, key, strconv.Itoa(i)+"_"+kkey, vvvvvalue)
 					}
 				}
 			}
 		}
+	case string:
+		switch key {
+		case
+			"engine",
+			"instanceID",
+			"instanceResourceID",
+			"timestamp",
+			"uptime":
+			// skipping those values
+		default:
+			return fmt.Errorf("unsupported key '%s' when collecting value", key)
+		}
+	default:
+		return fmt.Errorf("unsupported value type '%T' for key '%s' when collecting value", value, key)
 	}
+
+	return nil
 }
 
 func main() {
 	log.Infoln("Starting rdsosmetrics_exporter", version.Info())
 	log.Infoln("Build context", version.BuildContext())
 
-	exporter, err := NewExporter()
-	if err != nil {
-		log.Fatalln(err)
-	}
+	exporter := NewExporter()
 	prometheus.MustRegister(exporter)
 
 	log.Infoln("Started")
